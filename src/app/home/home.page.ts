da224431db088d526b122871f67be17e94443f4d
johnjackson <30298344@qq.com>
2020-10-23 13:45:17 +0800

    ionic项目应用保利威视频播放器demo
    ionic5 项目中应用保利威视频播放器（JS SDK）

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
 
+import VConsole from 'vconsole';
+new VConsole();
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  vid = 'fb3aa3b1d204200362e919cfd461f799_f';
+  vodPlayerJs = 'https://player.polyv.net/script/player.js';
+  player;
+  constructor() { }
 
-  constructor() {}
+  ngAfterViewInit() {
+    this.loadPlayerScript(this.loadPlayer.bind(this));
+  }
+
+  private loadPlayer() {
+    setTimeout(() => {
+      this.player = (window as any).polyvPlayer({
+        // forceH5: true,
+        wrap: '#player',
+        height: 250,
+        vid: this.vid,
+      });
+    }, 100);
+
+    (window as any).s2j_onFullScreen = () => {
+      console.log('进入全屏');
+    };
+
+    // 这个不工作
+    (window as any).s2j_onNormalScreen = () => {
+      console.log('退出全屏');
+    };
+
+  }
+
+  private loadPlayerScript(callback) {
+    if (!(window as any).polyvPlayer) {
+      const myScript = document.createElement('script');
+      myScript.onload = callback;
+      myScript.setAttribute('src', this.vodPlayerJs);
+      document.body.appendChild(myScript);
+    } else {
+      callback();
+    }
+  }
 
 }
